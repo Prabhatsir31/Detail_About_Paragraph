9120b334de53436a78cdd697566082a1566cee90
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2025-04-12 17:02:39 +0530

    Update Paragraph.java

--- Paragraph.java
+++ Paragraph.java
@@ -23,7 +23,7 @@ public class Paragraph {
                 consonant++;
             else if(ch[i]=='0' || ch[i]=='1' || ch[i]=='2' || ch[i]=='3' || ch[i]=='4' || ch[i]=='5' || ch[i]=='6' || ch[i]=='7' || ch[i]=='8' || ch[i]=='9')
                 digit++;
-            else if(ch[i]==' ')
+            else if(ch[i] == ' ')
                 space++;
             else 
                 symbol++;
